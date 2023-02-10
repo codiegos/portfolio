cc9c848d3640cf516496b22f2d4143464f42b56b
Diego Rivera <diegosebsk@gmail.com>
2023-02-10 15:36:31 -0300

    add custom breakpoints

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      xs: '390px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       backgroundImage: {
         'linear-soft': "url('src/assets/images/background.webp')",
