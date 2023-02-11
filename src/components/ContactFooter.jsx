5cd5bf3331588e4233ce753bfc7e5dd2b1cc4a09
Diego Rivera <diegosebsk@gmail.com>
2023-02-11 19:07:04 -0300

    add padding on contact footer texts

--- src/components/ContactFooter.jsx
+++ src/components/ContactFooter.jsx
@@ -4,13 +4,13 @@ const ContactFooter = ({ englishTranslate }) => {
   const { designed, repository } = englishTranslate;
   return (
     <div className="text-xs md:text-lg lg:text-xl w-full h-20 flex justify-between items-center border-t-2 dark:border-t-gray-700">
-      <span className="text-bold ml-4">
+      <span className="text-bold p-4">
         {designed || "Diseñado y Desarrollado por Diego Rivera"}
       </span>
       <a
         href="https://github.com/diego-zack/portfolio"
         target="_blank"
-        className="text-bold mr-4 underline hover:text-sky-400 duration-500"
+        className="text-bold p-4 underline hover:text-sky-400 duration-500"
       >
         {repository || "Revisar repositorio en Github"}
       </a>
