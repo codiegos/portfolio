472a91c6ad2f75b35804615845e3738d1ab177da
Diego Rivera <diegosebsk@gmail.com>
2023-02-11 19:15:35 -0300

    add modal info to projects section

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 
-const Modal = ({ open, text, onClose }) => {
+const Modal = ({ id, open, englishTranslate, onClose }) => {
+  const { turicuentro, djangoShop } = englishTranslate;
+
+  const text = (id === "turicuentro" && (turicuentro ||
+        "Proyecto Freelance realizado con React, Material-UI y Expressjs utilizando el ORM Sequalize. Enfocado en la administración de reservas de cabañas con despliegue en un calendario. Contempla autenticación con JWT y módulos de reserva, cliente, cabaña y algunas configuraciones extras.")) 
+        || (id === "djangoShop" && (djangoShop ||
+        "En este proyecto de universidad utilicé Python, Bootstrap V5 y Django. Enfocado en la creación de un carrito de compras utilizando django sessions, también posee un apartado autenticado para la administración de productos e usuarios."));
+
   const handleClose = (e) => {
-    e.target.id === "modal-projects" && onClose()
+    e.target.id === "modal-projects" && onClose();
   };
+
   return (
     <>
       {open && (
         <div
           id="modal-projects"
-          className="fixed z-10 inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center lg:hidden"
+          className="fixed z-10 inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center lg:px-[25%]"
           onClick={handleClose}
         >
-          <div className="bg-white p-8 rounded-lg w-auto text-justify dark:bg-black border-2 border-sky-400">
+          <div className="bg-white p-8 rounded-lg w-auto text-justify dark:bg-slate-900 border-2 border-sky-400 lg:text-xl">
             {text}
           </div>
         </div>
